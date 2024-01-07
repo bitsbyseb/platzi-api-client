3bad6593b2b5e902ec8a5a6eeb5714f943f342ad
Johan Puentes <puentespulidojohansebastian@gmail.com>
2024-01-07 18:10:14 -0500

    [OPTIMIZATION] improved fonts loading (using woff2 instead ttf,deleting google fonts cdns) and added minify for css and javascript

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base:'/platzi-api-client/'
+  base:'/platzi-api-client/',
+  build:{
+    cssMinify:true,
+    minify:true,
+  }
 });
